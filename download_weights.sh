3ed92c55b3865f11a6b3a45e8bdcd6a543ff38de
dgymjol <dgymjol@yonsei.ac.kr>
2023-04-28 12:39:19 +0900

    add clip prediction results

--- download_weights.sh
+++ download_weights.sh
@@ -2,7 +2,12 @@ mkdir -p pretrain
 
 python tools/maskclip_utils/convert_clip_weights.py --model RN50 --backbone
 python tools/maskclip_utils/convert_clip_weights.py --model ViT16 --backbone
+
 python tools/maskclip_utils/convert_clip_weights.py --model RN50
 python tools/maskclip_utils/convert_clip_weights.py --model ViT16
+
 python tools/maskclip_utils/prompt_engineering.py --model RN50 --class-set context
 python tools/maskclip_utils/prompt_engineering.py --model ViT16 --class-set context
+
+# for text module
+python tools/maskclip_utils/generate_text_features.py --model ViT32 --class-set context
---
+++ vessl.sh
@@ -0,0 +1,19 @@
+# export PATH=/root/anaconda3/bin:$PATH
+# conda init
+# conda activate maskclip
+apt-get update
+apt-get install sudo
+
+sudo apt-get update
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get install --only-upgrade libstdc++6
+
+sudo apt-get update
+
+sudo apt-get install libmagickwand-dev
+
+sudo apt-get update
+
+sudo apt-get install libgl1
